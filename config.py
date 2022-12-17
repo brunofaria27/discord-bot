0e321a75eced8c29b53ee5b00e881783df8ff37c
Bruno Rodrigues Faria <bruno.fariadevl@gmail.com>
2022-12-17 00:48:25 -0300

    welcome embed - melhorar visilibidade

---
+++ config.py
@@ -0,0 +1,4 @@
+token = 'MTA1MzQyMzA0MTkyNjkzMDQ2Mg.Gu01-N.XGTiwTVY7e_sYqszBc91OcefxGFeFR9h5A5Xls' #Put Your bots token here
+prefix = '!' #put prefix here
+link = 'https://discord.com/api/oauth2/authorize?client_id=1053423041926930462&permissions=8&scope=bot%20applications.commands'
+ownerid = '655579176132280321' #put your id here
